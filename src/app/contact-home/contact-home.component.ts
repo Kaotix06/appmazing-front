e660ff3919df28321ecda1276806d0dd2c4db7e4
Néstor Riveiro Martínez <nestor_rm06@hotmail.com>
2023-11-07 14:01:49 +0100

    /Second commit

--- src/app/contact-home/contact-home.component.ts
+++ src/app/contact-home/contact-home.component.ts
@@ -1,31 +1,28 @@
-import { Component } from "@angular/core";
-
-export interface Contact {
-  id: number;
-  name: string;
-  middlename: string;
-  lastname: string;
-  phone: string;
-  email: string;
-}
-
-const ELEMENT_DATA: Contact[] = [
-  {
-    id: 1,
-    name: "Néstor",
-    middlename: "Riveiro",
-    lastname: "Martínez",
-    phone: "659513549",
-    email: "nestor_rm06@hotmail.com",
-  },
-]
+import { Component, OnInit } from "@angular/core";
+import { ContactsService } from "../contacts.service";
 
 @Component({
   selector: "app-contact-home",
   styleUrls: ["./contact-home.component.css"],
   templateUrl: "./contact-home.component.html",
 })
-export class ContactHomeComponent {
-  displayedColumns: string[] = ["id", "name", "middlename", "lastname", "phone", "email"];
-  dataSource = ELEMENT_DATA;
+export class ContactHomeComponent implements OnInit {
+  contacts: any = [];
+
+  constructor(private contactsService: ContactsService){}
+
+  ngOnInit(): void {
+    this.contactsService.getContacts().subscribe(data =>{
+      this.contacts = data;
+    })
+  }
+
+  displayedColumns: string[] = [
+    "id",
+    "name",
+    "middlename",
+    "lastname",
+    "phone",
+    "email"
+  ];
 }
