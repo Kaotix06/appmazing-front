e745cb812db490c46421043f8ac3703b23cd6489
Néstor Riveiro Martínez <nestor_rm06@hotmail.com>
2023-11-14 12:05:42 +0100

    /update forms and edit completed

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoriesService } from '../categories.service';
+import { Category } from '../Model/Category';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
+  categories: any = [];
+  category: Category = new Category();
+  product: any;
 
-  constructor() { }
+  constructor(private productService: ProductsService, private route: ActivatedRoute, private router: Router, private categoriesServices: CategoriesService) { }
 
   ngOnInit() {
+    this.productService.getProduct(this.route.snapshot.params['id']).subscribe(data =>{
+      this.product = data;
+    })
+    this.loadCategories();
+  }
+
+  loadCategories(){
+    this.categoriesServices.getCategories().subscribe((data: Category[]) => {
+      this.categories = data;
+    });
+  }
+
+  updateProduct(){
+    this.productService.updateProduct(this.product);
+    this.navigateDetail();
+  }
+
+  cancelUpdate(){
+    this.navigateDetail();
+  }
+
+  navigateDetail(){
+    this.router.navigate(['/product', this.route.snapshot.params['id']]);
   }
 
 }
